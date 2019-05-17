3c54376d79f0f604c0115e55f296cc98ef6bada4
Peter Fagan <peterfagan@Peters-MacBook-Pro.local>
2019-05-17 13:30:30 +0100

    Add existing file

--- Model Fitting.R
+++ Model Fitting.R
@@ -5,6 +5,7 @@ library(gridExtra)
 library(xgboost)
 library(Matrix)
 library(caret)
+library(glmnet)
 
 library(parallel)
 library(doParallel)
@@ -68,6 +69,45 @@ fit_rf <- randomForest(x,y)
 importance(fit_rf)
 varImp(fit_rf)
 
+#XGBoost (Regression)
+#Grid search to tune parameters.
+#For the case of brevity of run time it has been minimised here.
+
+xgb_trcontrol = trainControl(
+  method = "cv",
+  number = 10,  
+  verboseIter = FALSE
+)
+
+xgbGrid <- expand.grid(nrounds = c(20,60,100),
+                       max_depth = 8,
+                       eta = c(0.01,0.02,0.05,0.1),
+                       gamma=0,
+                       min_child_weight = 1,
+                       colsample_bytree = 0.8,
+                       subsample = 1
+)
+
+cluster <- makeCluster(detectCores() - 1) #leave 1 core for Operating System
+registerDoParallel(cluster)
+
+set.seed(150)
+xgb_model = caret::train(
+  x, y,  
+  trControl = xgb_trcontrol,
+  tuneGrid = xgbGrid,
+  method = "xgbTree",
+  metric="RMSE",
+  allowParallel = TRUE
+)
+
+stopCluster(cluster)
+
+xgb_pred <- predict(xgb_model,x_t)
+(RMSE_xgb <- sqrt(mean((xgb_pred-target_test_sample)^2)))
+
+plot(xgb_model)
+
 
 #Neural Network (Regression)
 #Grid search to tune parameters.
@@ -79,13 +119,14 @@ nnet_trcontrol <- trainControl(
 )
 
 nnetGrid <- expand.grid(
-  size = 10,
-  decay = 0.01
+  size = c(5,10,15,20),
+  decay = c(0.01,0.05,0.1,0.2)
 )
 
 cluster <- makeCluster(detectCores() - 1) # convention to leave 1 core for OS
 registerDoParallel(cluster)
 
+set.seed(150)
 nnet_model = caret::train(
   x, y,  
   trControl = nnet_trcontrol,
@@ -100,64 +141,105 @@ nnet_model = caret::train(
 stopCluster(cluster)
 
 nnet_pred <- predict(nnet_model,x_t)
-(RMSE_nnet <- mean((nnet_pred-target_test_sample)^2))
+(RMSE_nnet <- sqrt(mean((nnet_pred-target_test_sample)^2)))
 
 plot(nnet_model)
 
 
-#XGBoost (Regression)
-#Grid search to tune parameters.
-#For the case of brevity of run time it has been minimised here.
 
-xgb_trcontrol = trainControl(
-  method = "cv",
-  number = 10,  
+#Lasso Regression
+
+glmnet_trcontrol <- trainControl(
+  method = 'cv', 
+  number = 10, 
   verboseIter = FALSE
 )
 
-xgbGrid <- expand.grid(nrounds = 100,
-                       max_depth = c(5,8,10),
-                       eta = 0.01,
-                       gamma=0,
-                       min_child_weight = 1,
-                       colsample_bytree = 0.8,
-                       subsample = 1
-)
+glmnetGrid <- expand.grid(
+  alpha =  seq(0,1,0.1),
+  lambda = seq(0.001,0.1,by = 0.001)
+  )
 
-cluster <- makeCluster(detectCores() - 1) #leave 1 core for Operating System
-registerDoParallel(cluster)
+registerDoSEQ()
+set.seed(150)
+glmnet_model <- train(
+  x, y, 
+  method = "glmnet", 
+  trControl = glmnet_trcontrol ,
+  preProcess=c("scale","center"),
+  tuneGrid = glmnetGrid)
 
-xgb_model = caret::train(
-  x, y,  
-  trControl = xgb_trcontrol,
-  tuneGrid = xgbGrid,
-  method = "xgbTree",
-  metric="RMSE",
-  allowParallel = TRUE
-)
+pred_glmnet <- predict(glmnet_model, newx = x_t, s = "lambda.min")
+(RMSE_glm <- sqrt(mean((target_test_sample-pred_glmnet)^2)))
 
-stopCluster(cluster)
-
-xgb_pred <- predict(xgb_model,x_t)
-(RMSE_xgb <- mean((xgb_pred-target_test_sample)^2))
-
-plot(xgb_model)
-
-#Lasso Regression
-glm_mod <- cv.glmnet(x, y, alpha = 1, family="gaussian",type.measure = "mse", nfolds = 10)
-pred_glm <- predict(glm_mod, newx = x_t, s = "lambda.min")
-(RMSE_glm <- sqrt(mean((target_test_sample-pred_glm)^2)))
 
 
 
 #Assessment of model performances
-#Disregard glmnet since it performs poorly compared to other models
-results <- resamples(list(xgboost=xgb_model, NeuralNetwork=nnet_model))
+results <- resamples(list(xgboost=xgb_model, NeuralNetwork=nnet_model,Lasso =glmnet_model ))
 results$values
 summary(results)
 bwplot(results)
 
-#Perform statistical test of samples e.g. students t test
+rmse_xgb = rmse_nnet = rmse_glmnet = numeric(10)
+folds = cut(1:10000,10,labels = FALSE)
+random_folds = sample(folds,10000)
+
+for(i in 1:10){
+  itrain = which(random_folds==i)
+  
+  x_train <- data.matrix(train[itrain,c(-1,-2)])
+  x_test <- data.matrix(train[-itrain,c(-1,-2)])
+  y_train <- train[itrain,1]
+  y_test <- train[-itrain,1]
+  
+  
+  cluster <- makeCluster(detectCores() - 1) #leave 1 core for Operating System
+  registerDoParallel(cluster)
+   
+   
+  xgb_model = caret::train(
+    x_train, y_train,  
+    method = "xgbTree",
+    metric="RMSE",
+    allowParallel = TRUE
+  )
+   
+  stopCluster(cluster)
+   
+  xgb_pred <- predict(xgb_model,x_test)
+  rmse_xgb[i] <- sqrt(mean((xgb_pred-y_test)^2))
+
+
+  cluster <- makeCluster(detectCores() - 1) # convention to leave 1 core for OS
+  registerDoParallel(cluster)
+   
+  nnet_model = caret::train(
+    x_train, y_train,  
+    method = "nnet",
+    metric="RMSE",
+    preProcess=c("scale","center")
+  )
+   
+  stopCluster(cluster)
+   
+  nnet_pred <- predict(nnet_model,x_test)
+  rmse_nnet[i] <- sqrt(mean((nnet_pred - y_test)^2))
+
+
+  registerDoSEQ()
+  glmnet_model <- train(
+    x_train, y_train, 
+    method = "glmnet"
+    )
+  
+  pred_glmnet <- predict(glmnet_model, newx = x_test)
+  rmse_glmnet[i] <- sqrt(mean((y_test-pred_glmnet)^2))
+  
+  
+}
+
+boxplot(rmse_xgb,rmse_nnet,rmse_glmnet,col = c("green","white","orange"),names=c("XGBoost","Neural Network","Lasso"))
 
 #Next we aim to improve our above predictions through additionally predicting the
 #likelihood of a customer returning. This will be treated as a binary classification,
@@ -196,7 +278,6 @@ stopCluster(cluster)
 nnetcl_pred <- predict(nnetcl_model,x_t,type="prob")
 tb = table(max.col(nnetcl_pred),ret_test_sample)
 (acc_nnetcl = sum(diag(tb))/sum(tb))
-
 
 #Lasso for Classification
 glmcl_mod <- cv.glmnet(x, y_ret, alpha = 1, family="binomial",type.measure = "auc", nfolds = 10)
